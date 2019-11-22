61cbd3e74552a32f194c2878b4936dac195f30ac
Saulo Mendes de Melo <saulo.melo@saudece.ce.gov.br>
2019-11-22 14:34:21 -0300

    dois picharts add, posicao ajustada

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
   //console.log(dataset)
+  
 
   facts = crossfilter(dataset)
   
@@ -15,12 +16,11 @@ d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
     taxaAno.all()[i].value = (taxaAno.all()[i].value / nascAno.all()[i].value) * 1000
   }
 
-  console.log(taxaAno.all())
-
   let lineChart = dc.lineChart(".line-chart-obitos")
-  let xScale = d3.scaleTime()
+  let xScale = d3.scaleLinear()
     .domain([yearDim.bottom(1)[0].ANO, yearDim.top(1)[0].ANO])
 
+  console.log(xScale)
   let yScaleSmall = d3.scaleLinear()
     .domain([0, 10])
   let yScale = d3.scaleLinear()
@@ -30,8 +30,10 @@ d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
     .width(800)
     .height(400)
     .dimension(yearDim)
-    .margins({ top: 50, right: 50, bottom: 25, left: 40 })
+    .margins({ top: 50, right: 50, bottom: 40, left: 50 })
     .renderArea(false)
+    .yAxisLabel("Nº de óbitos")
+    .xAxisLabel("Ano")
     .x(xScale)
     .renderHorizontalGridLines(true)
     .brushOn(false)
@@ -55,12 +57,14 @@ d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
   barChart
     .width(800)
     .height(400)
-    .margins({ top: 50, right: 50, bottom: 25, left: 40 })
+    .margins({ top: 50, right: 50, bottom: 40, left: 50 })
     .dimension(idadeDim)
     .x(d3.scaleBand())
     .xUnits(dc.units.ordinal)
     .brushOn(true)
     .group(obitoIdade)
+    .yAxisLabel("Nº de óbitos")
+    .xAxisLabel("Idade da Mãe")
 
   barChart.render()
 
@@ -105,10 +109,10 @@ d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
   let piechart = dc.pieChart(".pie-chart-raça")
 
   piechart
-    .width(768)
-    .height(480)
+    .width(250)
+    .height(200)
     .slicesCap(4)
-    .innerRadius(100)
+    .innerRadius(40)
     .dimension(racaDim)
     .group(obitoRaca)
   // .legend(dc.legend())
@@ -119,6 +123,56 @@ d3.csv('../mortalidade_datavis_sem2013.csv', function (dataset) {
   //     })
   // });
   piechart.render();
+
+  // ###########################################################################
+  // Gráfico que relaciona o tempo de escolaridade da mae à Quantidade de Óbitos
+  // ###########################################################################
+  escDim = facts.dimension(d => d.ESCMAE)
+
+  obitoEsc = escDim.group().reduceSum(d => d.OBITO)
+
+  let piechart2 = dc.pieChart(".pie-chart-escmae")
+
+  piechart2
+    .width(250)
+    .height(200)
+    .slicesCap(4)
+    .innerRadius(40)
+    .dimension(escDim)
+    .group(obitoEsc)
+  // .legend(dc.legend())
+  // // workaround for #703: not enough data is accessible through .label() to display percentages
+  // .on('pretransition', function(chart) {
+  //     chart.selectAll('text.pie-slice').text(function(d) {
+  //         return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+  //     })
+  // });
+  piechart2.render();
+
+  // ################################################################
+  // Gráfico que relaciona o tempo de gestacao à Quantidade de Óbitos
+  // ################################################################
+  gestDim = facts.dimension(d => d.GESTACAO)
+
+  obitoGest = gestDim.group().reduceSum(d => d.OBITO)
+
+  let piechart3 = dc.pieChart(".pie-chart-gestacao")
+
+  piechart3
+    .width(250)
+    .height(200)
+    .slicesCap(4)
+    .innerRadius(40)
+    .dimension(gestDim)
+    .group(obitoGest)
+  // .legend(dc.legend())
+  // // workaround for #703: not enough data is accessible through .label() to display percentages
+  // .on('pretransition', function(chart) {
+  //     chart.selectAll('text.pie-slice').text(function(d) {
+  //         return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+  //     })
+  // });
+  piechart3.render();
 
 
 });
